b70cf2a74907687f224894c39023c7f01e35fae1
hohohonghong <117703629+hohohonghong@users.noreply.github.com>
2024-03-02 16:06:06 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('animate');
+        }
+    });
+}, {
+    threshold: 0.5
+});
+
+document.querySelectorAll('section').forEach(section => {
+    observer.observe(section);
+});
